831e1df4f163cda6e0e27194598c7296321bb1d8
Vladimir <vladimirkristall@yandex.ru>
2022-08-11 00:45:57 +0300

    Add Task4_1 Second method

---
+++ Task4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// Второй способ решения задачи 13
+
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int index = 2;
+if(number[0] == '-') index ++;
+if(number.Length <= index)
+{
+    Console.WriteLine($"Нет третьей цифры в числе {number}");
+}
+else
+{
+    Console.WriteLine($"Третья цифра {number[index]}");
+}
