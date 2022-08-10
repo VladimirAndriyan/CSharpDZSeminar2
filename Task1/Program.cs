c8fca67276d8c91707648aa31649f9649dcbc19f
Vladimir <vladimirkristall@yandex.ru>
2022-08-10 23:40:02 +0300

    Add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 7 == 0 & number % 23 == 0)
 {
-Console.WriteLine("Да");
+    Console.WriteLine("Да");
 }
 else
 {
